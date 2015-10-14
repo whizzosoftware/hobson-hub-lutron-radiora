4f477b358a30a97654ab77f2a1a4a31fa19e5349
Daniel Noguerol <dan@whizzosoftware.com>
2015-10-13 21:41:55 -0600

    Fixed a ByteBuf leak.

--- src/main/java/com/whizzosoftware/hobson/radiora/api/codec/RadioRaFrameDecoder.java
+++ src/main/java/com/whizzosoftware/hobson/radiora/api/codec/RadioRaFrameDecoder.java
@@ -37,63 +37,70 @@ public class RadioRaFrameDecoder extends DelimiterBasedFrameDecoder {
 
     @Override
     protected Object decode(ChannelHandlerContext ctx, ByteBuf buffer) throws Exception {
-        logger.trace("decode: {}", buffer.toString(CharsetUtil.UTF_8));
+        if (logger.isTraceEnabled()) {
+            logger.trace("decode: {}", buffer.toString(CharsetUtil.UTF_8));
+        }
 
         ByteBuf frame = (ByteBuf)super.decode(ctx, buffer);
 
         if (frame != null) {
-            // if we receive a single byte frame, it should be a '!'
-            if (frame.readableBytes() == 1) {
-                byte b = frame.readByte();
-                if (b == '!') {
-                    return null;
+            try {
+                // if we receive a single byte frame, it should be a '!'
+                if (frame.readableBytes() == 1) {
+                    byte b = frame.readByte();
+                    if (b == '!') {
+                        return null;
+                    } else {
+                        throw new CorruptedFrameException("Unexpected single byte frame");
+                    }
+                    // otherwise, we assume it's a command frame
                 } else {
-                    throw new CorruptedFrameException("Unexpected single byte frame");
-                }
-            // otherwise, we assume it's a command frame
-            } else {
-                int ix = frame.indexOf(frame.readerIndex(), frame.writerIndex(), (byte) ',');
-                if (ix > 0) {
-                    String cmdName = frame.slice(0, ix).toString(CharsetUtil.UTF_8);
+                    int ix = frame.indexOf(frame.readerIndex(), frame.writerIndex(), (byte) ',');
+                    if (ix > 0) {
+                        String cmdName = frame.slice(0, ix).toString(CharsetUtil.UTF_8);
 
-                    switch (cmdName) {
-                        case ZoneMap.TYPE:
-                            if (frame.readableBytes() >= ix + 33) {
-                                return new ZoneMap(frame.slice(ix + 1, 32).toString(CharsetUtil.UTF_8));
-                            } else {
-                                throw new CorruptedFrameException("Received invalid zone map size");
-                            }
+                        switch (cmdName) {
+                            case ZoneMap.TYPE:
+                                if (frame.readableBytes() >= ix + 33) {
+                                    return new ZoneMap(frame.slice(ix + 1, 32).toString(CharsetUtil.UTF_8));
+                                } else {
+                                    throw new CorruptedFrameException("Received invalid zone map size");
+                                }
 
-                        case LocalZoneChange.TYPE:
-                            if (frame.readableBytes() >= ix + 3) {
-                                String sZoneNum = frame.slice(ix + 1, 2).toString(CharsetUtil.UTF_8);
-                                if (frame.readableBytes() >= ix + 7) {
-                                    String state = frame.slice(ix + 4, 3).toString(CharsetUtil.UTF_8).trim();
-                                    try {
-                                        return new LocalZoneChange(Integer.parseInt(sZoneNum), LocalZoneChange.State.valueOf(state));
-                                    } catch (IllegalArgumentException iae) {
-                                        throw new CorruptedFrameException("Invalid LZC state string");
+                            case LocalZoneChange.TYPE:
+                                if (frame.readableBytes() >= ix + 3) {
+                                    String sZoneNum = frame.slice(ix + 1, 2).toString(CharsetUtil.UTF_8);
+                                    if (frame.readableBytes() >= ix + 7) {
+                                        String state = frame.slice(ix + 4, 3).toString(CharsetUtil.UTF_8).trim();
+                                        try {
+                                            return new LocalZoneChange(Integer.parseInt(sZoneNum), LocalZoneChange.State.valueOf(state));
+                                        } catch (IllegalArgumentException iae) {
+                                            throw new CorruptedFrameException("Invalid LZC state string");
+                                        }
+                                    } else {
+                                        throw new CorruptedFrameException("Invalid LZC size (state)");
                                     }
                                 } else {
-                                    throw new CorruptedFrameException("Invalid LZC size (state)");
+                                    throw new CorruptedFrameException("Invalid LZC size (zoneNum)");
                                 }
-                            } else {
-                                throw new CorruptedFrameException("Invalid LZC size (zoneNum)");
-                            }
 
-                        case LEDMap.TYPE:
-                            if (frame.readableBytes() >= ix + 16) {
-                                return new LEDMap(frame.slice(ix + 1, 15).toString(CharsetUtil.UTF_8));
-                            } else {
-                                throw new CorruptedFrameException("Invalid LED map size");
-                            }
+                            case LEDMap.TYPE:
+                                if (frame.readableBytes() >= ix + 16) {
+                                    return new LEDMap(frame.slice(ix + 1, 15).toString(CharsetUtil.UTF_8));
+                                } else {
+                                    throw new CorruptedFrameException("Invalid LED map size");
+                                }
 
-                        default:
-                            throw new DecoderException("Unrecognized command: " + cmdName);
+                            default:
+                                throw new DecoderException("Unrecognized command: " + cmdName);
+                        }
+                    } else {
+                        throw new CorruptedFrameException("Invalid frame format (no comma)");
                     }
-                } else {
-                    throw new CorruptedFrameException("Invalid frame format (no comma)");
                 }
+            } finally {
+                // we consumed the frame, so we need to release it
+                frame.release();
             }
         } else {
             return null;
